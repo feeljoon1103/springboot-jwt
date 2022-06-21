126f098150290c157b795bd0232d61ebe0143841
feeljoon1193 <feeljoon1103@gmail.com>
2022-06-21 23:05:32 +0900

    init

--- src/main/java/com/bookflex/backend/controller/UserController.java
+++ src/main/java/com/bookflex/backend/controller/UserController.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
